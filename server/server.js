3653869a0a88b35fb9f5b2a8b9f3af1c19c812c7
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-06-02 15:36:37 +0200

    fix: changement de port de la partie serveur

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./router");
 
 const server = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3002;
 
 server.use(router);
 

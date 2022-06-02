0f4089f41a8dd05cfaf9800caa9c0285597de414
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-06-02 22:10:15 +0200

    Logique d'illustration dynamique de pollution

---
+++ client/src/components/Illustration/Illustration.js
@@ -0,0 +1,26 @@
+import "./illustration.css";
+import pollution from "./../../assets/img/pollution.jpg";
+import average from "./../../assets/img/average.jpg";
+import turquoise from "./../../assets/img/turquoise.jpg";
+
+function Illustration({ data }) {
+  if (data <= 20) {
+    return (
+      <img className="main__rightBoard-pic" src={pollution} alt="eau polluée" />
+    );
+  } else if (data > 20 && data < 80) {
+    return (
+      <img
+        className="main__rightBoard-pic"
+        src={average}
+        alt="eau mi-figue mi-raisin"
+      />
+    );
+  } else {
+    return (
+      <img className="main__rightBoard-pic" src={turquoise} alt="eau claire" />
+    );
+  }
+}
+
+export default Illustration;

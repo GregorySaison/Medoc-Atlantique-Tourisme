42a9637a050b9f22f521d1393d9ca40fe0376fcb
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-06-02 17:41:04 +0200

    Paramétrage par défaut des élements conteneur de l'application

---
+++ client/src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./app.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__title">Pollution des Plages</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;

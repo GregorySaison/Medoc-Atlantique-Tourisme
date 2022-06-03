5646a35ece1715485e8f97d4d7ba61505223207c
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-06-03 13:30:37 +0200

    Ajout de la nouvelle route pour aller chercher les données supplémentaires

--- server/router.js
+++ server/router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const displayController = require("./controllers/displayController");
 
 router.get("/hourtin", displayController.getHourtin);
+router.get("/others", displayController.getAllOthers);
 
 module.exports = router;

3b24ee318d3fc65a5be5af6fca30be69aae37178
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-06-05 18:09:19 +0200

    refacto: MAJ du composant Table

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -4,17 +4,7 @@ import "./table.css";
 import Api from "./../../utils/baseUrl";
 
 function Table() {
-  const [name, setName] = useState("");
-  const [rate, setRate] = useState(0);
   const [others, setOthers] = useState([]);
-
-  // useEffect(() => {
-  //   Api.get("/hourtin").then((res) => {
-  //     const response = res.data;
-  //     setName(response[0].name);
-  //     setRate(response[0].rate);
-  //   });
-  // }, []);
 
   useEffect(() => {
     Api.get("/cities").then((res) => {
@@ -25,10 +15,6 @@ function Table() {
 
   return (
     <ul className="footer__list">
-      {/* <li className="footer__listItem">
-        <span className="listItem__name">{name}</span>
-        <span className="listItem__rate">{rate} %</span>
-      </li> */}
       {others.map((item) => (
         <li key={item.name} className="footer__listItem">
           <span className="listItem__name">{item.name}</span>
